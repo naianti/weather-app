64bf81f24148572cc6e338e0153d66d937b28280
naianti <87500932+naianti@users.noreply.github.com>
2021-07-25 03:31:11 -0400

    added function display default temperature city

--- src/index.js
+++ src/index.js
@@ -1,114 +1,51 @@
-//Task #1
-//It display the current date and time
-let currentTime = new Date();
-
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
-
-let hour = currentTime.getHours();
-let minutes = currentTime.getMinutes();
-let day = days[currentTime.getDay()];
-let date = currentTime.getDate();
-let month = currentTime.getMonth() + 1; //+1 to give format to month
-let year = currentTime.getFullYear();
-
-let LocalTime = document.querySelector(".LocalTime");
-LocalTime.innerHTML = `${hour}:${minutes}`;
-
-let Day = document.querySelector(".ConcurrentDay");
-Day.innerHTML = `${day} ${date}/${month}/${year}`;
-
-//Task #3
-//Display Fahrenheit.
-function showFahrenheit(event) {
-  event.preventDefault();
-  let temperatureFahrenheit = document.querySelector("#temp-concurrent");
-  temperatureFahrenheit.innerHTML = 76;
-}
-let fahrenheit = document.querySelector("#fahrenheit");
-fahrenheit.addEventListener("click", showFahrenheit);
-
-//Display celsius by default
-
-function showCelsius(event) {
-  event.preventDefault();
-  let converToCelsius = document.querySelector("#temp-concurrent");
-  let celsiusTemperature = `${converToCelsius}`;
-  converToCelsius.innerHTML = 7852;
-}
-let celsius = document.querySelector("#celsius");
-celsius.addEventListener("click", showCelsius);
-
-//end Display celsius by default
-
-//Task search engine, show the city name and displays the weather on the page after the user submits the form.
-
-function showTemperature(response) {
-  console.log(response);
-  let temperature = Math.round(response.data.main.temp);
-  let cityRequest = `${response.data.name}, ${response.data.sys.country}`;
-  let cityRequestWeatherDescription = `${response.data.weather[0].description}`;
-
-  let localTemperature = document.querySelector("#temp-concurrent");
-  let h1 = document.querySelector("h1");
-  let h3 = document.querySelector("h3");
-
-  localTemperature.innerHTML = `${temperature}`;
-  h1.innerHTML = `${cityRequest}`;
-  h3.innerHTML = `${cityRequestWeatherDescription}`;
+function formatDay(timestamp) {
+  let date = new Date(timestamp);
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  return `${day}`;
 }
 
-function searchEngine(event) {
-  event.preventDefault();
-  let searchInput = document.querySelector("#search-text-input");
-  let cityName = `${searchInput.value}`;
-  let apiKey = "0c0f15195845da49d19b504381bfef7a";
-  let units = "metric";
-  let apiUrlSearch = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}&units=${units}`;
-
-  axios.get(apiUrlSearch).then(showTemperature);
+function formatTime(timestamp) {
+  let date = new Date(timestamp);
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${hours}:${minutes}`;
 }
 
-let formSearcher = document.querySelector("form");
-formSearcher.addEventListener("submit", searchEngine);
+function showDefaulCity(response) {
+  console.log(response.data);
 
-//Task geolocation
-function showLocalTemperature(response) {
-  let temperature = Math.round(response.data.main.temp);
-  let localCity = `${response.data.name}, ${response.data.sys.country}`;
-  let localWeatherDescription = `${response.data.weather[0].description}`;
-
-  let h1 = document.querySelector("h1");
-  let localTemperature = document.querySelector("#temp-concurrent");
-  let h3 = document.querySelector("h3");
-
-  localTemperature.innerHTML = `${temperature}`;
-  h1.innerHTML = `${localCity}`;
-  h3.innerHTML = `${localWeatherDescription}`;
+  let defaultCity = document.querySelector("#city-name");
+  let tempElement = document.querySelector("#concurrent-temp");
+  let descriptionElement = document.querySelector("h3");
+  let feelingElement = document.querySelector("#feeling");
+  let clockElement = document.querySelector("#clock");
+  let dayWeekElement = document.querySelector("#concurrent-day");
+  defaultCity.innerHTML = `${response.data.name}, ${response.data.sys.country}`;
+  tempElement.innerHTML = Math.round(response.data.main.temp);
+  descriptionElement.innerHTML = response.data.weather[0].description;
+  feelingElement.innerHTML = Math.floor(response.data.main.feels_like);
+  clockElement.innerHTML = formatTime(response.data.dt * 1000);
+  dayWeekElement.innerHTML = formatDay(response.data.dt * 1000);
 }
 
-function showLocation(position) {
-  let latitude = position.coords.latitude;
-  let longitude = position.coords.longitude;
-  let units = "metric";
-  let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
-  let apiKey = "0c0f15195845da49d19b504381bfef7a";
-  let apiUrl = `${apiEndpoint}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+let defaultCity = "Caracas, VE";
+let apiKey = "0c0f15195845da49d19b504381bfef7a";
+let units = "metric";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${defaultCity}&appid=${apiKey}&units=${units}`;
 
-  axios.get(apiUrl).then(showLocalTemperature);
-}
-
-function geolocationRequest(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(showLocation);
-}
-
-let localRequest = document.querySelector(".button-Location");
-localRequest.addEventListener("click", geolocationRequest);
+axios.get(apiUrl).then(showDefaulCity);
